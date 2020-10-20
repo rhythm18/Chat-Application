fe0b69cd597600570fa1befa8bca78e266c5d86f
Rhythm Arya <rhythmarya@Rhythms-MacBook-Air.local>
2020-10-20 17:50:45 +0530

    UI updated

--- public/chat.js
+++ public/chat.js
@@ -23,7 +23,7 @@ message.addEventListener('keypress', function(){
 // Listen for events
 socket.on('chat', function(data){
     feedback.innerHTML = '';
-    output.innerHTML += '<p><strong>' + data.handle + ': </strong>' + data.message + '</p>';
+    output.innerHTML += '<p>  <strong>' + data.handle + ': </strong>' + data.message + '</p>';
 });
 
 socket.on('typing', function(data){
